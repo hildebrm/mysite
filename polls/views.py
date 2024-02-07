43e4c4057a43347ed4bfe1510dca407f7f167c64
maxxHildebrand <mbhildebrand6@gmail.com>
2024-02-07 12:02:53 -0500

    part 3

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Question
-
+#hi
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
